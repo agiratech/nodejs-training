bcefedee7d44f40bf37971de984aee3caccace52
logakrishnan <logakrishnan@agiratech.com>
2018-02-27 18:53:35 +0530

    first basic program using url module

---
+++ 2018-02-27/basic-url.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var url = require('url');
+
+
+var server = http.createServer(function(req, res) {
+
+var page = url.parse(req.url).pathname;
+
+console.log(page);
+
+res.writeHead(200, {"Content-Type": "text/plain"});
+
+res.write('first program using url module');
+
+res.end();
+
+});
+
+server.listen(8080);
+
