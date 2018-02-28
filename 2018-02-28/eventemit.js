90300b323cc5267757f90d9bac81bd16f9d1d898
logakrishnan <logakrishnan@agiratech.com>
2018-02-28 19:32:45 +0530

    basic program of event emit and event listening

---
+++ 2018-02-28/eventemit.js
@@ -0,0 +1,19 @@
+var EventEmitter = require('events').EventEmitter; 
+
+
+var game = new EventEmitter(); 
+
+
+game.on('gameover', function(message)
+
+
+{
+
+
+ console.log(message); 
+
+
+}); 
+
+
+game.emit('gameover', 'You lose!');
