bce64c567a4e35639475f602df7d72f53e849868
logakrishnan <logakrishnan@agiratech.com>
2018-03-07 19:50:51 +0530

    sample buffer program

---
+++ 2018-03-07/buffer1.js
@@ -0,0 +1,13 @@
+const buf1 = Buffer.alloc(10);
+const buf2 = Buffer.alloc(14);
+const buf3 = Buffer.alloc(18);
+const totalLength = buf1.length + buf2.length + buf3.length;
+
+console.log(totalLength);
+
+const bufA = Buffer.concat([buf1, buf2, buf3], totalLength);
+
+console.log(bufA);
+
+console.log(bufA.length);
+
