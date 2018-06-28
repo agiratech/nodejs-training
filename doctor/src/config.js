7452782d91e5561826426aa02a643cb3b0e9e7dd
aishwarya <aishwarya@agiratech.com>
2018-06-28 11:31:27 +0530

    Deleted old files

--- doctor/src/config.js
+++
@@ -1,3 +0,0 @@
-export const 
-GOOGLE_API_KEY = "AIzaSyDyg_Po6CuP6EA6yvYjRsGj8x2nGqdn8pc",
-CALENDAR_ID = "sqm2udi1adf6ds7gj9h81tuet8@group.calendar.google.com"
--- hospital_api/hospital/server/routes/index.route.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var doctorRoute = require( './doctor.route')
-var billingRoute = require('./billing.route')
-router.use('/doctor',doctorRoute);
-router.use('/billing', billingRoute);
-
-module.exports = router;
